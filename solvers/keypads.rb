b991432305530433ee70aa36a27ceeae1c0a6de0
devanhurst <devan.hurst@gmail.com>
2016-03-01 18:32:59 -0500

    Solve keypads

---
+++ solvers/keypads.rb
@@ -0,0 +1,61 @@
+@choices = ['q', 'a', 'lambda', 'lightning', 'alien', 'h', 'backwards c', 'e',
+          'curly', 'whitestar', 'question', 'copyright', 'butt', 'i', 'r',
+          '6', 'paragraph', 'p', 'smiley', 'trident', 'c', 'blackstar', 'snake',
+          'hash', 'ae', 'n', 'omega']
+
+@solutions = [['q','a','lambda','lightning','alien','h','c'],
+              ['e','q','c','curly','whitestar','h','question'],
+              ['copyright','butt','curly','i','r','lambda','whitestar'],
+              ['6','paragraph','p','alien','i','question','smiley'],
+              ['trident','smiley','p','c','paragraph','snake','blackstar'],
+              ['6','e','hash','ae','trident','n','omega']]
+@symbols = []
+@final_answer = []
+
+def solve(symbols, count)
+  system "clear" or system "cls"
+  if count > 5
+    puts "INVALID SYMBOL SET. TRY AGAIN"
+    sleep(1)
+    @symbols = []
+    prompt_user
+  else
+    @symbols.each do |symbol|
+      if @solutions[count].include?(symbol)
+        @final_answer.push(symbol)
+      end
+    end
+    if @final_answer.count == 4
+      @solutions[count].each do |solution_item|
+        if @symbols.include?(solution_item)
+          puts solution_item.upcase
+        end
+      end
+      @bomb.back_to_menu
+    else
+      @final_answer = []
+      solve(@symbols, count + 1)
+    end
+  end
+end
+
+def prompt_user
+  system "clear" or system "cls"
+  puts "SYMBOL LIST"
+  @choices.each {|choice| puts choice}
+  count = 1
+  while count < 5
+    print "Symbol " + count.to_s + ": "
+    symbol = gets.chomp
+      while @choices.include?(symbol) == false || @symbols.include?(symbol)
+        puts "Invalid choice. Try again."
+        print "Symbol " + count.to_s + ": "
+        symbol = gets.chomp
+      end
+    @symbols.push(symbol)
+    count += 1
+  end
+  solve(@symbols, 0)
+end
+
+prompt_user
