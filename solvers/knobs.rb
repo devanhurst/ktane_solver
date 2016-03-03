55ff17ed71dec3cb9124e1fb407fbd5eaf1113de
devanhurst <devan.hurst@gmail.com>
2016-03-03 16:36:18 -0500

    Solved Knobs

---
+++ solvers/knobs.rb
@@ -0,0 +1,27 @@
+def solve(input)
+  case input
+  when 'ddbdub', 'udbnbd'
+    puts "Knob position: UP"
+  when 'dbbdnb', 'udunud'
+    puts "Knob position: DOWN"
+  when 'dnndbd', 'nnndbd'
+    puts "Knob position: LEFT"
+  when 'bdbubu', 'bdbudn'
+    puts "Knob position: RIGHT"
+  when 'q'
+    @bomb.back_to_menu
+  else
+    puts 'Invalid input, try again.'
+    prompt_user
+  end
+  @bomb.back_to_menu
+end
+
+def prompt_user
+  puts "Enter the light sequence. (Down: 'd', Up: 'u', Both: 'b', None: 'n')"
+  puts "q to quit"
+  solve(gets.chomp)
+end
+
+system "cls" or system "clear"
+prompt_user
